3f1e45a1dc5d29bea01793546d328d9819b80736
Alex Knight <alex.knight@leavittpartners.com>
2019-08-09 11:39:00 -0600

    Now getting data from API

--- src/App.js
+++ src/App.js
@@ -1,64 +1,96 @@
 import React from 'react';
 import ForecastDay from './components/ForecastDay';
+import axios from 'axios'
 import './App.css';
 
-const forecastDummy = [
-    {
-        maxTemp: 99,
-        minTemp: 67,
-        humidity: 50,
-        wind: {
-            speed: 13,
-            direction: 129
-        },
-        weatherPic: 'LightRain'
-    },
-    {
-        maxTemp: 95,
-        minTemp: 56,
-        humidity: 50,
-        wind: {
-            speed: 13,
-            direction: 278
-        },
-        weatherPic: 'ThunderyShowers'
-    },
-    {
-        maxTemp: 99,
-        minTemp: 67,
-        humidity: 50,
-        wind: {
-            speed: 13,
-            direction: 129
-        },
-        weatherPic: 'ThunderyHeavyRain'
-    },
-    {
-        maxTemp: 99,
-        minTemp: 67,
-        humidity: 50,
-        wind: {
-            speed: 13,
-            direction: 129
-        },
-        weatherPic: 'Sunny'
-    },
-    {
-        maxTemp: 99,
-        minTemp: 67,
-        humidity: 50,
-        wind: {
-            speed: 13,
-            direction: 129
-        },
-        weatherPic: 'HeavyShowers'
-    }
-]
+// const forecastDummy = [
+//     {
+//         maxTemp: 99,
+//         minTemp: 67,
+//         humidity: 50,
+//         wind: {
+//             speed: 13,
+//             direction: 129
+//         },
+//         weatherPic: 'LightRain'
+//     },
+//     {
+//         maxTemp: 95,
+//         minTemp: 56,
+//         humidity: 50,
+//         wind: {
+//             speed: 13,
+//             direction: 278
+//         },
+//         weatherPic: 'ThunderyShowers'
+//     },
+//     {
+//         maxTemp: 99,
+//         minTemp: 67,
+//         humidity: 50,
+//         wind: {
+//             speed: 13,
+//             direction: 129
+//         },
+//         weatherPic: 'ThunderyHeavyRain'
+//     },
+//     {
+//         maxTemp: 99,
+//         minTemp: 67,
+//         humidity: 50,
+//         wind: {
+//             speed: 13,
+//             direction: 129
+//         },
+//         weatherPic: 'Sunny'
+//     },
+//     {
+//         maxTemp: 99,
+//         minTemp: 67,
+//         humidity: 50,
+//         wind: {
+//             speed: 13,
+//             direction: 129
+//         },
+//         weatherPic: 'HeavyShowers'
+//     }
+// ]
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                this.loadWeatherDataCoord(position.coords.latitude, position.coords.longitude);
+            });
+        }
+
+        this.state = {weatherData: null, zipcode: '', validZip: false};
+    }
+
+    loadWeatherData(zipcode) {
+        axios.get('https://10qqyrptbi.execute-api.us-west-2.amazonaws.com/default/forecast-function', {params:{zipcode: zipcode}, crossdomain: true})
+            .then(response => this.setState({weatherData: response.data.days}));
+    }
+
+    loadWeatherDataCoord(lat, lon) {
+        axios.get('https://10qqyrptbi.execute-api.us-west-2.amazonaws.com/default/forecast-function', {params:{lat: lat, lon: lon}, crossdomain: true})
+            .then(response => this.setState({weatherData: response.data.days}));
+
+    }
+
     render() {
+        let {zipcode, weatherData, validZip} = this.state;
+
         return <div className="App">
-            {forecastDummy.map((day, i) => {
+            <div id="location">
+                <input type="text" placeholder="Zip code" onChange={(e) => this.setState({zipcode: e.target.value, validZip: /^[0-9]{5}$/.test(e.target.value)})} value={zipcode} ></input>
+                <button disabled={!validZip} onClick={() => this.loadWeatherData(zipcode)} >Go</button>
+            </div>
+            {!!weatherData && weatherData.map((day, i) => {
+                console.log(day);
                 return <ForecastDay dayNum={i} forecast={day} key={i} />
             })}
         </div>
--- src/components/ForecastDay.jsx
+++ src/components/ForecastDay.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 //Source: https://github.com/schachmat/wego/blob/master/frontends/ascii-art-table.go
 const asciiWeather = {
     "Unknown": "    .-.      </br>     __)     </br>    (        </br>     `-᾿     </br>      •      ",
-    "Cloudy": "             </br>     .--.    </br>  .-(    ).  </br> (___.__)__) </br>             ",
-    "Fog": "             </br> _ - _ - _ - </br>  _ - _ - _  </br> _ - _ - _ - </br>             ",
-    "HeavyRain": "     .-.     </br>    (   ).   </br>   (___(__)  </br><span class=\"blue\">  ‚ʻ‚ʻ‚ʻ‚ʻ   </br>  ‚ʻ‚ʻ‚ʻ‚ʻ   </span>",
-    "HeavyShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br><span class=\"blue\">   ‚ʻ‚ʻ‚ʻ‚ʻ  </br>   ‚ʻ‚ʻ‚ʻ‚ʻ  </span>",
-    "HeavySnow": "     .-.     </br>    (   ).   </br>   (___(__)  </br>   * * * *   </br>  * * * *    ",
+    "03d": "             </br>     .--.    </br>  .-(    ).  </br> (___.__)__) </br>             ",
+    "50d": "             </br> _ - _ - _ - </br>  _ - _ - _  </br> _ - _ - _ - </br>             ",
+    "09d": "     .-.     </br>    (   ).   </br>   (___(__)  </br><span class=\"blue\">  ‚ʻ‚ʻ‚ʻ‚ʻ   </br>  ‚ʻ‚ʻ‚ʻ‚ʻ   </span>",
+    "10d": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br><span class=\"blue\">   ‚ʻ‚ʻ‚ʻ‚ʻ  </br>   ‚ʻ‚ʻ‚ʻ‚ʻ  </span>",
+    "13d": "     .-.     </br>    (   ).   </br>   (___(__)  </br>   * * * *   </br>  * * * *    ",
     "HeavySnowShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>    * * * *  </br>   * * * *   ",
     "LightRain": "     .-.     </br>    (   ).   </br>   (___(__)  </br><span class=\"lightblue\">    ʻ ʻ ʻ ʻ  </br>   ʻ ʻ ʻ ʻ   </span>",
     "LightShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br><span class=\"lightblue\">     ʻ ʻ ʻ ʻ </br>    ʻ ʻ ʻ ʻ  </span>",
@@ -15,36 +15,36 @@ const asciiWeather = {
     "LightSleetShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>     ʻ * ʻ * </br>    * ʻ * ʻ  ",
     "LightSnow": "     .-.     </br>    (   ).   </br>   (___(__)  </br>    *  *  *  </br>   *  *  *   ",
     "LightSnowShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>     *  *  * </br>    *  *  *  ",
-    "PartlyCloudy": "<span class=\"yellow\">   \\  /      </span></br><span class=\"yellow\"> _ /\"\"</span>.-.    </br><span class=\"yellow\">   \\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>             ",
-    "Sunny": "<span class=\"yellow\">    \\   /    </br>     .-.     </br>  ‒ (   ) ‒  </br>     `-᾿     </br>    /   \\    </span>",
-    "ThunderyHeavyRain": "     .-.     </br>    (   ).   </br>   (___(__)  </br><span class=\"blue\">  ‚ʻ⚡ʻ‚⚡‚ʻ   </br>  ‚ʻ‚ʻ⚡ʻ‚ʻ   </span>",
+    "02d": "<span class=\"yellow\">   \\  /      </span></br><span class=\"yellow\"> _ /\"\"</span>.-.    </br><span class=\"yellow\">   \\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>             ",
+    "01d": "<span class=\"yellow\">    \\   /    </br>     .-.     </br>  ‒ (   ) ‒  </br>     `-᾿     </br>    /   \\    </span>",
+    "11d": "     .-.     </br>    (   ).   </br>   (___(__)  </br><span class=\"blue\">  ‚ʻ⚡ʻ‚⚡‚ʻ   </br>  ‚ʻ‚ʻ⚡ʻ‚ʻ   </span>",
     "ThunderyShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>    ⚡ʻ ʻ⚡ʻ ʻ </br>    ʻ ʻ ʻ ʻ  ",
     "ThunderySnowShowers": "<span class=\"yellow\"> _`/\"\"</span>.-.    </br><span class=\"yellow\">  ,\\_</span>(   ).  </br><span class=\"yellow\">   /</span>(___(__) </br>     *⚡ *⚡ * </br>    *  *  *  ",
-    "VeryCloudy": "             </br>     .--.    </br>  .-(    ).  </br> (___.__)__) </br>             "
+    "04d": "             </br>     .--.    </br>  .-(    ).  </br> (___.__)__) </br>             "
 }
 
 function ForecastDay(props) {
-    let {maxTemp, minTemp, wind, humidity, weatherPic} = props.forecast;
+    let {temp_max, temp_min, wind_dir, wind_speed, humidity, weather_icon_id} = props.forecast;
 
     return <div className="day">
         <div id="maxTemp">
-            {maxTemp}
+            {temp_max}
         </div>
         <div id="minTemp">
-            {minTemp}
+            {temp_min}
         </div>
         <div id="wind">
-            <div id="windDirection" style={{transform: `rotate(${wind.direction}deg)`}}>
+            <div id="windDirection" style={{transform: `rotate(${wind_dir}deg)`}}>
                 ->
             </div>
             <div id="windSpeed">
-                {wind.speed}mph
+                {Math.round(wind_speed)}mph
             </div>
         </div>
         <div id="humidity">
-            {humidity}%
+            {Math.round(humidity)}%
         </div>
-        <pre id="pic" dangerouslySetInnerHTML={{__html: asciiWeather[weatherPic]}}>
+        <pre id="pic" dangerouslySetInnerHTML={{__html: asciiWeather[weather_icon_id.replace('n','d')]}}>
         </pre>
     </div>
 }
